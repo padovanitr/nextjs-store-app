554b83aa20d16e29953e915b82e774aa95769717
Thiago Padovani <padovanitr@gmail.com>
2023-07-03 16:17:44 -0300

    finish configuring styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './globals.css'
 import { Roboto } from 'next/font/google'
+import StyledComponentsRegistry from '@/lib/registry'
 
 const inter = Roboto({
   subsets: ['latin'],
@@ -19,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+        </body>
     </html>
   )
 }
